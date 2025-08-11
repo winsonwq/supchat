3a6faaa9ec42bed246316205c93fce32654d7c17
Wang Qiu <winsonwq@gmail.com>
2025-08-12 02:00:55 +0800

    add spin and update refresh button

---
+++ miniprogram/components/spin/spin.ts
@@ -0,0 +1,29 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    spinning: {
+      type: Boolean,
+      value: false
+    },
+    size: {
+      type: Number,
+      value: 24
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
